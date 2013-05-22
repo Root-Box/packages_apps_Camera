0978c6843b0642a0bae685217758849c1fa4544b
Pascal Pollet <paspol@gmx.de>
2013-05-23 00:11:27 +0200

    Camera: Add a GPS indicator
    This will add a GPS indicator to the preview of the photo camera.
    The indicator is placed as an overlay at the upper top corner
    of the photo camera preview.
    
    It shows one of three icons depending on the GPS state:
        - acquiring fix
        - GPS fix acquired
        - GPS disallowed in system settings
    
    The icon will only be shown in the preview of the photo module,
    not in the video or panorama module, and not in the gallery view.
    
    Drawables by blunden.
    
    Change-Id: I0686ef4ee1fe9b34ac9beaaa2bd43555234f4f4d

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -210,6 +210,8 @@ public class PhotoModule
     private ImageView mNoHandsIndicator;
     private TextView mTimerCountdown;
 
+    private ImageView mGpsIndicator;
+
     // We use a thread in ImageSaver to do the work of saving images. This
     // reduces the shot-to-shot time.
     private ImageSaver mImageSaver;
@@ -837,14 +839,33 @@ public class PhotoModule
         mSceneIndicator = (ImageView) mOnScreenIndicators.findViewById(R.id.menu_scenemode_indicator);
         mHdrIndicator = (ImageView) mOnScreenIndicators.findViewById(R.id.menu_hdr_indicator);
         mNoHandsIndicator = (ImageView) mRootView.findViewById(R.id.indicator_nohandsshot);
+        mGpsIndicator = (ImageView) mRootView.findViewById(R.id.indicator_gps);
         mTimerCountdown = (TextView) mRootView.findViewById(R.id.timer_countdown);
     }
 
     @Override
-    public void showGpsOnScreenIndicator(boolean hasSignal) { }
+    public void showGpsOnScreenIndicator(boolean enabled, boolean hasSignal) {
+        if (mGpsIndicator == null || !mActivity.isInCameraApp()) {
+            return;
+        }
+
+        if (!enabled) {
+            mGpsIndicator.setImageResource(R.drawable.ic_viewfinder_gps_off);
+        } else if (hasSignal) {
+            mGpsIndicator.setImageResource(R.drawable.ic_viewfinder_gps_on);
+        } else {
+            mGpsIndicator.setImageResource(R.drawable.ic_viewfinder_gps_no_signal);
+        }
+        mGpsIndicator.setVisibility(View.VISIBLE);
+    }
 
     @Override
-    public void hideGpsOnScreenIndicator() { }
+    public void hideGpsOnScreenIndicator() {
+        if (mGpsIndicator == null) {
+            return;
+        }
+        mGpsIndicator.setVisibility(View.GONE);
+    }
 
     private void updateExposureOnScreenIndicator(int value) {
         if (mExposureIndicator == null) {
@@ -1496,6 +1517,7 @@ public class PhotoModule
         if (ApiHelper.HAS_SURFACE_TEXTURE) {
             if (mActivity.mCameraScreenNail != null) {
                 ((CameraScreenNail) mActivity.mCameraScreenNail).setFullScreen(full);
+                hideGpsOnScreenIndicator();
             }
             return;
         }
@@ -1596,6 +1618,9 @@ public class PhotoModule
         if (mHandler.hasMessages(SHOW_TAP_TO_FOCUS_TOAST)) {
             mHandler.removeMessages(SHOW_TAP_TO_FOCUS_TOAST);
             showTapToFocusToast();
+        }
+        if (mLocationManager != null) {
+            mLocationManager.updateGpsIndicator();
         }
     }
 
@@ -2267,6 +2292,7 @@ public class PhotoModule
             setCameraState(PREVIEW_STOPPED);
             mFocusManager.onCameraReleased();
             stopSmartCapture();
+            hideGpsOnScreenIndicator();
         }
     }
 
